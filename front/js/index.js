b064e0c9298f90ccf427b4f2f7b16076992cf56a
Magali Pascaud <magtopie@gmail.com>
2022-01-03 09:29:24 +0100

    premier repo

---
+++ front/js/index.js
@@ -0,0 +1,9 @@
+console.log("je suis dans l'accueil");
+//ce fichier doit aller chercher la liste de produits depuis le serveur
+function getProducts(){
+
+}
+//puis afficher les produits dans la section items
+function displayProducts(){
+    
+}
