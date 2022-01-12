a2658a2985237dd706b15cd4109edfa845682b6b
Magali Pascaud <magtopie@gmail.com>
2022-01-12 19:54:58 +0100

    index.js ok

--- front/js/index.js
+++ front/js/index.js
@@ -1,9 +1,39 @@
 console.log("je suis dans l'accueil");
+getProducts()
+    .then(function (products) {
+        displayProducts(products)
+    })
 //ce fichier doit aller chercher la liste de produits depuis le serveur
-function getProducts(){
-
+function getProducts() {
+    return fetch('http://localhost:3000/api/products')
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                console.log("Erreur")
+            }
+        })
+        .then(function (value) {
+            return value;
+        })
+        .catch(function (err) {
+            //Erreur
+        });
 }
 //puis afficher les produits dans la section items
-function displayProducts(){
-    
+function displayProducts(products) {
+    const itemsSection = document.getElementById("items");
+    for (const product of products) {
+        console.log(product);
+        const link = document.createElement("a");
+        link.href = `./product.html?id=${product._id}`;
+        link.innerHTML = `<article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>`;
+        itemsSection.appendChild(link)
+
+    }
 }
--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,37 @@
 console.log('je suis dans le produit');
+//récupéer l'ID présent 
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+//récupérer le produit sélectionné
+function getProduct(){
+    return fetch('http://localhost:3000/api/products')
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+        else {
+            console.log("Erreur")
+        }
+    })
+    .then(function (value) {
+        return value;
+    })
+    .catch(function (err) {
+        //Erreur
+    });
+}
+
+//mettre les infos au bon endroit dans le html
+
+
+
+//gérer les évenements de sélection de couleur
+
+
+
+//gérer les évenements de quantité
+
+
+
+//gérer l'évenement d'ajout de panier
