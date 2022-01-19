e300781c6afc9e07f38e04a3d72bfd18231af6bb
Magali Pascaud <magtopie@gmail.com>
2022-01-19 14:19:13 +0100

    travail sur couleur

--- front/js/product.js
+++ front/js/product.js
@@ -6,6 +6,7 @@ const productId = urlParams.get('id');
 getProduct()
     .then(function (product) {
         displayProduct(product)
+        colorProduct(product)
     })
 
 //récupérer le produit sélectionné
@@ -28,36 +29,45 @@ function getProduct() {
 }
 
 //mettre les infos au bon endroit dans le html
-function displayProduct(productId) {
+function displayProduct(product) {
+    console.log(product);
 
-    console.log(productId);
-    document
-        .getElementsByClassName("item__img")
-        .innerHTML = `<img src="${productId.imageUrl}" alt="${productId.altTxt}">`;
+    const title = document.getElementById("title");
+    title.textContent = `${product.name}`;
 
-    document
-        .getElementById("title")
-        .innerHTML = `<h1 id="title">${productId.name}</h1>`;
-    document
-        .getElementById("price")
-        .innerHTML = `<span id="price">${productId.price}</span>`;
-    document
-        .getElementById("description")
-        .innerHTML = `<p id="description">${productId.description}</p>`;
+    const price = document.getElementById("price");
+    price.textContent = `${product.price}`
 
+    const description = document.getElementById("description");
+    description.textContent = `${product.description}`
 
+    const image = document.getElementById("image");
+    image.src = `${product.imageUrl}`;
+    image.alt = `${product.altTxt}`;
 
+    const colors = document.getElementById("colors");
+    for (const color of product.colors){
+        const option = document.createElement("option");
+        option.textContent = color;
+        option.value = color;
+        colors.appendChild(option);
+    }
+    colors.addEventListener('change',onColorChange);
 
-
+    const quantity = document.getElementById("quantity");
+    quantity.addEventListener('change',onQuantityChange);
 }
 
 
 //gérer les évenements de sélection de couleur
-
-
-
+function onColorChange(event) {
+    console.log(event);
+    
+}
 //gérer les évenements de quantité
-
+function onQuantityChange(event) {
+    console.log(event);
+}
 
 
 //gérer l'évenement d'ajout de panier
